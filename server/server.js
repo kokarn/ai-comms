4a5ce117880fe1140d97c774d583ecc8094cf515
kokarn <kokarn@gmail.com>
2017-05-24 12:18:57 +0200

    Add server support for client identifiers

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const io = socketio( server );
 // const db = nano.use( 'games' );
 app.use( express.static( path.join( __dirname, 'web' ) ) );
 
-const connections = [];
+const connections = {};
 const finishedGames = {};
 const runningGames = {};
 const waitingGames = {};
@@ -44,38 +44,34 @@ const checkGames = function checkGames(){
     }
 };
 
-const findGame = function findGame( gameType, clientId, opponent ){
+const findGame = function findGame( gameType, client, requiredOpponent ){
     if ( waitingGames[ gameType ].length <= 0 ) {
         return false;
     }
 
     for ( let i = 0; i < waitingGames[ gameType ].length; i = i + 1 ) {
         let currentGame = waitingGames[ gameType ][ i ];
+
         // If we have a game with no players, use that
         if ( currentGame.players.length < 1 ) {
-            if ( opponent ) {
-                currentGame.requestOpponent( opponent );
+            if ( requiredOpponent ) {
+                currentGame.requestOpponent( requiredOpponent );
             }
 
             return currentGame;
         }
 
         // If we have a game with an opponent as this client id, use that
-        if ( currentGame.opponent === clientId ) {
+        if ( currentGame.opponent === client.id ) {
             return currentGame;
         }
 
-        // If we have a game with an opponent as this client id, use that
-        if ( !currentGame.opponent ) {
-            if ( currentGame.players && opponent ) {
-                for ( let playerIndex = 0; playerIndex < currentGame.players.length; playerIndex = playerIndex + 1 ) {
-                    if ( currentGame.players[ playerIndex ].id === opponent ) {
-                        return currentGame;
-                    }
+        // Check if one of the games has a player defined as this requiredOpponent without another defined opponent
+        if ( !currentGame.opponent && requiredOpponent ) {
+            for ( let playerIndex = 0; playerIndex < currentGame.players.length; playerIndex = playerIndex + 1 ) {
+                if ( currentGame.players[ playerIndex ].id === requiredOpponent ) {
+                    return currentGame;
                 }
-
-                // If we haven't returned before this, it's not matching
-                return false;
             }
 
             return currentGame;
@@ -86,7 +82,7 @@ const findGame = function findGame( gameType, clientId, opponent ){
 };
 
 io.on( 'connection', ( client ) => {
-    connections.push( client.id );
+    connections[ client.id ] = client.id;
 
     console.log( `Client ${ client.id } connected` );
 
@@ -96,6 +92,10 @@ io.on( 'connection', ( client ) => {
         console.log( `got ${ data }` );
     } );
 
+    client.on( 'identifier', ( identifier ) => {
+        connections[ identifier ] = client.id;
+    } );
+
     client.on( 'join-game', ( gameConfig ) => {
         if( !games[ gameConfig.type ] ) {
             return false;
@@ -103,13 +103,13 @@ io.on( 'connection', ( client ) => {
 
         checkGames();
 
-        let game = findGame( gameConfig.type, client.id, gameConfig.opponent );
+        let game = findGame( gameConfig.type, client, connections[ gameConfig.opponent ] || gameConfig.opponent );
 
         if ( !game ) {
             game = new games[ gameConfig.type ];
 
             if ( gameConfig.opponent ) {
-                game.requestOpponent( gameConfig.opponent );
+                game.requestOpponent( connections[ gameConfig.opponent ] || gameConfig.opponent );
             }
 
             waitingGames[ gameConfig.type ].push( game );
@@ -137,7 +137,13 @@ io.on( 'connection', ( client ) => {
 
         checkGames();
 
-        connections.splice( connections.indexOf( client.id ), 1 );
+        delete connections[ client.id ];
+
+        for ( const identifier in connections ) {
+            if ( connections[ identifier ] === client.id ) {
+                delete connections[ identifier ];
+            }
+        }
     } );
 } );
 
